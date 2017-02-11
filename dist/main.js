7c1614db70e7ee78f5ebd7a7500d998b337824fa
Jesper Nilsson <jesper@rodolfo.se>
2017-02-11 18:04:56 +0100

    En liten till commit

--- dist/main.js
+++ dist/main.js
@@ -3,12 +3,23 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 var wordResponses = [
+  { word: 'hej', type: 'response', responses: ['Hallå där', 'Hejsan', 'Tjena'] },
   { word: 'hungrig', type: 'response', responses: ['Du borde ta en cigg!', 'Du menar hangry!', 'Drick vatten!'] },
   { word: 'törstig', type: 'response', responses: ['Ät mat!', 'Ät godis!'] },
   { word: 'wifi', type: 'response', responses: ['Titta ut!', 'Ta bussen!']},
-  { word: 'oslo', type: 'wiki' },
+  { word: 'stockholm', type: 'wiki' },
+  { word: 'internet', type: 'wiki' },
+  { word: 'oslo', type: 'wiki'},
+  { word: 'köpenhamn', type: 'wiki'},
+  { word: 'Boston', type: 'wiki' },
+  { word: 'Polis', type: 'wiki' },
 ];
 
+var wikiPrefix = [
+  'Det råkar vara så att jag kan en del om',
+  'Vänta, jag kan berätta om',
+  'Visste du förresten att',
+];
 
 var getWiki = (text) => fetch('/api/?query=' + text)
     .then((response) => response.text());
@@ -44,13 +55,18 @@ function newSpeechRecognition() {
         console.log(index);
         console.log(wordResponse.responses[index]);
         speak(wordResponse.responses[index]);
-      } else {
+      } else if(wordResponse.type === 'wiki') {
         getWiki(wordResponse.word)
           .then(response => {
             var firstSentence = response.split('.')[0];
-            console.log(firstSentence);
-            speak(firstSentence)
+
+            const index = Math.floor(Math.random() * wikiPrefix.length);
+
+            speak(`${wikiPrefix[index]} ${firstSentence}`);
           });
+      } else if(wordResponse.type === 'color') {
+        document.querySelector('body').style.backgroundColor = wordResponse.color;
+
       }
     }
   }
@@ -61,30 +77,43 @@ function newSpeechRecognition() {
   }
 
   recognition.onnomatch = function(event) {
-    diagnostic.textContent = "I didn't recognise that color.";
+    console.log('No match')
+    // diagnostic.textContent = "I didn't recognise that color.";
   }
 
   recognition.onerror = function(event) {
-    diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+    console.log('Error')
+    // diagnostic.textContent = 'Error occurred in recognition: ' + event.error
   }
 
-  recognition.start();  
+  recognition.start();
 }
 
 var voice;
 window.speechSynthesis.onvoiceschanged = function(e) {
-  voice = speechSynthesis.getVoices().find(function (voice) {
+  const voices = speechSynthesis.getVoices().find((voice) => {
+    // console.log(voice.lang)
     return voice.lang === 'sv-SE';
   });
+
+  voice = voices[0];
 };
 
 function speak(text) {
   var msg = new SpeechSynthesisUtterance();
   msg.text = text;
   msg.voice = voice;
-  console.log(voice);
+  // console.log(voice);
+
+  // msg.onend = (e) => console.log('Finished talking');
+
+
   document.querySelector('.text_to_speech').innerHTML = text;
   window.speechSynthesis.speak(msg);
+
+  msg.onend = function(event) {
+    console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+  }
 }
 
 window.addEventListener('load', function () {
